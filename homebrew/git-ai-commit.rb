0f4f639d74523aca308605a3a1d6a6dacb3d728e
Bikz <bikrambrar25@gmail.com>
2026-01-11 18:06:30 -0500

    chore: update homebrew checksums

--- homebrew/git-ai-commit.rb
+++ homebrew/git-ai-commit.rb
@@ -6,10 +6,10 @@ class GitAiCommit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Bikz/git-ai-commit/releases/download/v0.1.3/git-ai-commit-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "f4a6abf975462b724cd9cf39d3a1aecdc31a211d88ada6a3cb92891e0cb6b397"
     else
       url "https://github.com/Bikz/git-ai-commit/releases/download/v0.1.3/git-ai-commit-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "30848d3d3461a39f0aa9ae4d6dca681250763943f94d8c258b54dd3de8337463"
     end
   end
 
@@ -19,7 +19,7 @@ class GitAiCommit < Formula
     end
 
     url "https://github.com/Bikz/git-ai-commit/releases/download/v0.1.3/git-ai-commit-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "REPLACE_ME"
+    sha256 "9d45d7ac0be7e59ae66a15b03b1c043ecf1fc469210aa65a05ce2006e5bea046"
   end
 
   def install
