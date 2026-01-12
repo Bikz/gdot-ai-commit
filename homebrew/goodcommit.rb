ad5d44361193a821f0d421c1345ed5fbf5b69a37
Bikz <bikrambrar25@gmail.com>
2026-01-11 23:58:08 -0500

    chore: update brew checksums for v0.2.1

--- homebrew/goodcommit.rb
+++ homebrew/goodcommit.rb
@@ -6,10 +6,10 @@ class Goodcommit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Bikz/goodcommit/releases/download/v0.2.1/goodcommit-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "a3619932cb4f4d14fd02b29cd63e4d0a33f61065534b59e3374b2ebd49089272"
     else
       url "https://github.com/Bikz/goodcommit/releases/download/v0.2.1/goodcommit-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "3cff154ad011aae88e8aab751286ca5c23e38b15c8cd9f2a0a0f9e67b0f04dba"
     end
   end
 
@@ -19,7 +19,7 @@ class Goodcommit < Formula
     end
 
     url "https://github.com/Bikz/goodcommit/releases/download/v0.2.1/goodcommit-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "REPLACE_ME"
+    sha256 "a120278f3ebaeb4c7e132ab590f6bf3a907890c1149276daf5297275bb856dbb"
   end
 
   def install
