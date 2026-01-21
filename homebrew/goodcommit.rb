34a9a5d36d7393940681c5a05b96083f59218aa9
Bikz <bikrambrar25@gmail.com>
2026-01-21 01:01:28 -0500

    chore(release): v0.2.10

--- homebrew/goodcommit.rb
+++ homebrew/goodcommit.rb
@@ -1,14 +1,14 @@
 class Goodcommit < Formula
   desc "Good Commit: fast, reliable AI commit messages"
   homepage "https://github.com/Bikz/goodcommit"
-  version "0.2.9"
+  version "0.2.10"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Bikz/goodcommit/releases/download/v0.2.9/goodcommit-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/Bikz/goodcommit/releases/download/v0.2.10/goodcommit-aarch64-apple-darwin.tar.gz"
       sha256 "REPLACE_ME"
     else
-      url "https://github.com/Bikz/goodcommit/releases/download/v0.2.9/goodcommit-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/Bikz/goodcommit/releases/download/v0.2.10/goodcommit-x86_64-apple-darwin.tar.gz"
       sha256 "REPLACE_ME"
     end
   end
@@ -18,7 +18,7 @@ class Goodcommit < Formula
       odie "linux arm64 builds are not yet available"
     end
 
-    url "https://github.com/Bikz/goodcommit/releases/download/v0.2.9/goodcommit-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/Bikz/goodcommit/releases/download/v0.2.10/goodcommit-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "REPLACE_ME"
   end
 
